4b44c6be25ee8c757deb331871a69cac25dc7d56
EgorShanin <94217235+EgorShanin@users.noreply.github.com>
2022-03-09 21:01:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,29 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int count = 0;
+  int num = 0;
+  for (int i = 0; i < size; i++) {
+      for (int j = (i + 1); j < size; j++) {
+          if (arr[i] > arr[j]) {
+              num = arr[i];
+              arr[i] = arr[j];
+              arr[j] = num;
+          }
+      }
+  }
+  if (arr[size / 2] > value) {
+      for (int i = 0; i < ((size / 2)); i++) {
+          if (arr[i] == value) {
+              count++;
+          }
+      }
+  } else {
+      for (int i = (size / 2); i < size; i++) {
+          if (arr[i] == value) {
+              count++;
+          }
+      }
+  }
+  return count;
 }
